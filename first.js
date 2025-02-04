5ad081ca68c286c6b5eacaeac65eff760133caca
kar123-demo <karthiklaxman21@gmail.com>
2025-02-04 19:15:00 +0530

    update

--- first.js
+++ first.js
@@ -1,38 +1,65 @@
-const v=document.getElementById("background-video");
-v.onended=function(){
-    v.onpause();
+const v = document.getElementById("background-video");
+v.onended = function() {
+    v.pause(); 
 };
 
 
 function showImage() {
-    let imageContainer =
-        document.createElement("div");
-    imageContainer.className =
-        "image-container";
+    let imageContainer = document.createElement("div");
+    imageContainer.className = "image-container";
 
-    let image =
-        document.createElement("img");
-    image.src ="instructions.png";
-    let closeButton =
-        document.createElement(
-            "button"
-        );
-    closeButton.textContent = "removclose";
-    closeButton.className =
-        "close-btn";
-    closeButton.onclick =
-        function () {
-            document.body.removeChild(
-                imageContainer
-            );
-        };
+    let image = document.createElement("img");
+    image.src = "instructions.png";
 
-    imageContainer.appendChild(
-        closeButton
-    );
+    let closeButton = document.createElement("button");
+    closeButton.textContent = "Close";
+    closeButton.className = "close-btn";
+    closeButton.onclick = function () {
+        document.body.removeChild(imageContainer); 
+    };
+
+    imageContainer.appendChild(closeButton);
     imageContainer.appendChild(image);
 
-    document.body.appendChild(
-        imageContainer
-    );
+    document.body.appendChild(imageContainer);
+}
+
+
+function red() {
+    document.getElementById("b1").textContent = "play:Easy";
+}
+
+
+function red1() {
+    document.getElementById("b1").textContent = "play:Hard";
+}
+
+function red2() {
+    document.getElementById("b1").textContent = "play:Medium";
+}
+
+
+function demo() {
+    let buttonText = document.getElementById("b1").textContent;
+    if (buttonText === "play") { 
+        window.alert("Please select difficulty");
+    } else {
+        window.open('harish.html', '_blank'); 
+    }
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
