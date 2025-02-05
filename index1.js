a327001f8dd372e323e7fb8652e12554be485831
kar123-demo <karthiklaxman21@gmail.com>
2025-02-05 10:47:53 +0530

    Add files via upload

--- index1.js
+++ index1.js
@@ -1,7 +1,24 @@
+let v=100;
+let moves=0;
+let c=0;
+setInterval(()=>{
+  document.getElementById("t1").textContent="timer:"+v;
+  v--;
+},1500);
 function flipCard(card) {
     const cardInner = card.querySelector('.card-inner');
     cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)'
       ? 'rotateY(0deg)'
-      : 'rotateY(180deg)';   
+      : 'rotateY(180deg)';  
+       
+  
+   
+    moves++;
+  
+    if(moves%2==0) {
+      c++;
+    document.getElementById("m1").textContent = `Moves: ${c}`;
+  }
   }
   
+   
